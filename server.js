6222275408eeec65e1a213b031f80eff5004f2ba
PABLO EMMANUEL RONDEAU <33339221@comunidadort.edu.ar>
2020-06-10 21:59:42 -0300

    Cambios Server.js

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ import express from 'express'
 const app = express(),
 port = process.env.PORT || 3000;
 
-app.listen(port);
+const app = express()
 
-console.log('todo list RESTful API server started on: ' + port);
+app.use(express.json())
+
+let estudiantes = []
+
+app.get('/api/', (req,res) => {
+    console.log('get')
+   
+})
+
+app.post('/api/', (req,res) => {
+    console.log('post')
+    
+})
+
+app.put('/api//:id', (req,res) => {
+    console.log('put')
+    
+})
+
+app.delete('/api//:id', (req,res) => {
+    console.log('delete')
+
+})
+
+app.listen(port, error => {
+    if(error) throw new Error(`Error en servidor ${error}`)
+    console.log(`Servidor express escuchando en el puerto ${PORT}`)
+})
