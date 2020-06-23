4528e60bf88ece6fb495be607892163c4018d35f
Dario Di Gulio <ddigulio@technisys.com>
2020-06-23 13:44:18 -0300

    MVC strcuture done

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,49 +1,50 @@
-import Project from '../models/projectModel.js'
-const projectModel = new Project()
+import Project from "../models/projectModel.js";
+const projectModel = new Project();
 
 /**
-* @description Obtener un proyecto a través de su id
-* @param {Recibe el id de un proyecto existente}
-* @validate Valida que el id del proyecto corresponda a uno existente
-* @returns {En caso de éxito devuelve un objeto proyecto con su nombre, descripción y listas}
-*/
+ * @description Obtener un proyecto a través de su id
+ * @param {Recibe el id de un proyecto existente}
+ * @validate Valida que el id del proyecto corresponda a uno existente
+ * @returns {En caso de éxito devuelve un objeto proyecto con su nombre, descripción y listas}
+ */
 async function getProjectById(id) {
-    return 'Hola mundo'
+  return "Hola mundo";
 }
 
 /**
-* @description Crear un proyecto
-* @param {Recibe el nombre del nuevo proyecto}
-* @param {Recibe una descripción del nuevo proyecto}
-* @validate Valida que el nombre del proyecto no exista previamente
-* @returns {En caso de éxito devuelve un objeto proyecto con su nombre y descripción}
-*/
+ * @description Crear un proyecto
+ * @param {Recibe el nombre del nuevo proyecto}
+ * @param {Recibe una descripción del nuevo proyecto}
+ * @validate Valida que el nombre del proyecto no exista previamente
+ * @returns {En caso de éxito devuelve un objeto proyecto con su nombre y descripción}
+ */
 async function createProject(name, description) {
-    return 'Hola mundo'
+  return "Hola mundo";
 }
 
 async function getProjectStats(id) {
-    return 'Hola mundo'
+  return "Hola mundo";
 }
 
 async function getProjectStatsById(id) {
-    return 'Hola mundo'
+  return "Hola mundo";
 }
 
 async function getProjectActionHistory(id) {
-    return 'Hola mundo'
+  return "Hola mundo";
 }
 
-async function getLists(id) {
-    return 'Hola mundo'
+function getLists(res, req) {
+  // const lists = await projectModel.getList(req.body.idProject);
+  // return JSON.stringify(lists);
+  return "hola mudno"
 }
-
 
 export default {
-    getProjectById,
-    createProject,
-    getProjectStats,
-    getProjectStatsById,
-    getProjectActionHistory,
-    getLists
-}
+  getProjectById,
+  createProject,
+  getProjectStats,
+  getProjectStatsById,
+  getProjectActionHistory,
+  getLists,
+};
--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -1,5 +1,30 @@
+import knex from "../../DB/db_connection.js";
+
 class Project {
-    /* ver métodos en el controller */
+  /* ver métodos en el controller */
+  // getProjectById
+  // createProject
+  // getProjectStats
+  // getProjectStatsById
+  // getProjectActionHistory
+
+  async getList(idProject) {
+    try {
+      await knex
+        .select()
+        .from("list")
+        .where("id_project", idProject)
+        .then((rows) => {
+            if (rows.length > 0) {
+                return JSON.stringify(rows);
+            } else {
+                return "Empty project"
+            }
+        });
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
-export default Project
+export default Project;
