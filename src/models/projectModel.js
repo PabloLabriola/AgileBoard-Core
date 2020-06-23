8fad3b2016bcd19aea81472b4b370c9cb4a564dc
PABLO EMMANUEL RONDEAU <33339221@comunidadort.edu.ar>
2020-06-23 02:17:27 -0300

    se agregan los modelos de datos v1

--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -1,5 +1,21 @@
 class Project {
     /* ver métodos en el controller */
+    constructor(id, admin, name) {
+        
+        let _id = id
+        let _admin = admin
+        let _name = name       
+
+        this.getId = () => _id
+        this.setId = id => _id = id
+    
+        this.getAdmin = () => _admin
+        this.setAdmin = admin => _admin = admin
+    
+        this.getName = () => _name
+        this.setName = name => _name = name           
+
+    }   
 }
 
 export default Project
