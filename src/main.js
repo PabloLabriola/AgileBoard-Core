b4f9fa6d83a61b70e9852295fe1903b84a40afc9
Pablo Labriola <54651847+PabloLabriola@users.noreply.github.com>
2020-06-23 19:33:50 -0300

    main maintenance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import createApp from "./server/app.js";
 import _ from "underscore";
 import router from "./routes/index.js";
+import db from '../DB/db_connection.js'
 
 const app = createApp();
 const port = process.env.PORT || 3000;
@@ -16,10 +17,11 @@ app.use(function (req, res) {
   res.send("La ruta a la que intentás acceder o el recurso que solicitas no existe.");
 });
 
+db.createDB()
+
 const server = app.listen(port, () => {
   console.log(`Server started on: http://localhost:${server.address().port}`);
 });
 
-// import array from './query_test.js'
 
-// console.log(array)
+
