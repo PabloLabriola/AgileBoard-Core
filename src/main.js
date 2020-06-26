288ff915ef1e99acd6a513d584af2743c1427734
DarioDiGulio <39631247+DarioDiGulio@users.noreply.github.com>
2020-06-26 20:40:08 -0300

    dotenv structure

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-import createApp from "./server/app.js";
 import _ from "underscore";
 import router from "./routes/index.js";
-import db from '../DB/db_connection.js'
+import db from '../DB/db_connection.js';
+import express from 'express';
 
-const app = createApp();
+const app = express()
 const port = process.env.PORT || 3000;
+
+app.use(express.json())
 app.use("/", router);
 
 app.get("/", function (req, res) {
   res.send("<h1>Api en construcción</h1>");
 });
 
-// Error para rutas inexistentes
 app.use(function (req, res) {
   res.status(404);
   res.send("La ruta a la que intentás acceder o el recurso que solicitas no existe.");
